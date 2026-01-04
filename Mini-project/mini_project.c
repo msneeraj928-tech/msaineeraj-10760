02678a1c028852d0eedcc427e63a7737e712fccf
msneeraj928-tech <msneeraj928@gmail.com>
2026-01-04 14:15:22 +0530

    Mini project submission

--- Mini-project/mini_project.c
+++ Mini-project/mini_project.c
@@ -17,17 +17,18 @@ int main() {
     else
         printf("Odd\n");
 
-    if (n <= 1)
+    if (n <= 1) {
         prime = 0;
-
-    for (i = 2; i <= n - 1; i++) {
-        if (n % i == 0) {
-            prime = 0;
-            break;
+    } else {
+        for (i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                prime = 0;
+                break;
+            }
         }
     }
 
-    if (prime == 1)
+    if (prime)
         printf("Prime\n");
     else
         printf("Not Prime\n");
